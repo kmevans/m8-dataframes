9987f8c30b3291bc2fd65d794325ca21a9234c39
Kyle Evans <kmevans@uw.edu>
2016-10-11 13:58:24 -0700

    Wrote some code

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -2,25 +2,28 @@
 
 
 # Create a vector of everything you ate for breakfast
-
+breakfast <- c("Bagel", "Peanut butter", "Milk");
 
 # Create a vector of everything you ate for lunch
-
+lunch <- c("Cheeseburger", "French fries");
 
 # Create a list `meals` that has contains your breakfast and lunch
-
+meals <- list(breakfast=breakfast, lunch=lunch);
 
 # Add a `dinner` index to your `meals` list that has what you plan to eat for dinner
-
+meals$dinner <- c("Chicken", "Rice");
 
 # Extract your 'dinner' element from your list and save it in a vector called 'dinner'
-
+dinner <- meals[['dinner']];
 
 ### Bonus ### 
 # Create a list that has the number of items you ate for each meal
-
+items <- lapply(meals, length);
 
 # Write a function that adds pizza to every meal
-
+AddPizza <- function(input) {
+  input <- c(input, "pizza");
+}
 
 # Add pizza to every meal!
+lots.of.pizza <- lapply(meals, AddPizza);
